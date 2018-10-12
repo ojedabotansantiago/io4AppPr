44105d7ac7f4706e6dbc926bd09edd00f3da6aae
sojeda@funcionasi.es <sojeda@funcionasi.es>
2018-10-12 15:32:03 +0200

    change root page to login

--- src/app/login/login.module.ts
+++ src/app/login/login.module.ts
@@ -3,15 +3,15 @@ import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { ContactPage } from './contact.page';
+import { LoginPage } from './login.page';
 
 @NgModule({
-	imports: [
-		IonicModule,
-		CommonModule,
-		FormsModule,
-		RouterModule.forChild([{ path: '', component: ContactPage }])
-	],
-	declarations: [ContactPage]
+  imports: [
+    IonicModule,
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild([{ path: '', component: LoginPage }])
+  ],
+  declarations: [LoginPage]
 })
-export class ContactPageModule {}
+export class LoginPageModule {}
--- src/app/tabs/tabs.router.module.ts
+++ src/app/tabs/tabs.router.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { TabsPage } from './tabs.page';
+import { LoginPage } from './../login/login.page';
 import { HomePage } from '../home/home.page';
 import { AboutPage } from '../about/about.page';
 import { ContactPage } from '../contact/contact.page';
@@ -13,13 +14,18 @@ const routes: Routes = [
     children: [
       {
         path: '',
-        redirectTo: '/tabs/(home:home)',
-        pathMatch: 'full',
+        redirectTo: '/tabs/(login:login)',
+        pathMatch: 'full'
       },
       {
         path: 'home',
         outlet: 'home',
         component: HomePage
+      },
+      {
+        path: 'login',
+        outlet: 'login',
+        component: LoginPage
       },
       {
         path: 'about',
@@ -35,7 +41,7 @@ const routes: Routes = [
   },
   {
     path: '',
-    redirectTo: '/tabs/(home:home)',
+    redirectTo: '/tabs/(login:login)',
     pathMatch: 'full'
   }
 ];
